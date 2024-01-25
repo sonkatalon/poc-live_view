c7ead8b1fbe898dc239bf471e142462d042de363
Dao Hoang Son <daohoangson@gmail.com>
2024-01-25 10:40:25 +0700

    Disable the first run dialog

--- docker/device/entrypoint.sh
+++ docker/device/entrypoint.sh
@@ -2,6 +2,9 @@
 
 set -euo pipefail
 
+mkdir -p /home/selenium/.config/google-chrome
+touch '/home/selenium/.config/google-chrome/First Run'
+
 xvfb-run -l -n "$DISPLAY_NUM" -s "-ac -screen 0 $SCREEN_RESOLUTION -noreset -listen tcp" /usr/bin/fluxbox -display "$DISPLAY" -log /dev/null 2>/dev/null &
 
 x11vnc -display "$DISPLAY" -shared -forever -loop500 -rfbport 5900 -rfbportv6 5900 -logfile /dev/null &
