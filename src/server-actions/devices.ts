bb6ddc456f12ab1ea7e08ad5dd6702a4e53c08ea
Dao Hoang Son <daohoangson@gmail.com>
2024-01-29 01:34:19 +0700

    Join the same network to enable communication from extension -> Nextjs

--- src/server-actions/devices.ts
+++ src/server-actions/devices.ts
@@ -1,10 +1,13 @@
 "use server";
 
-import { spawn } from "child_process";
+import { exec, spawn } from "child_process";
+import { promisify } from "util";
 import { Device } from "@/models";
 
 let lastHostPort = 3000;
 const killers: Record<number, Function> = {};
+
+const execAsync = promisify(exec);
 
 export async function createDevice({
   hostname,
@@ -22,21 +25,30 @@ export async function createDevice({
   // make sure a valid URL is provided
   new URL(url);
 
+  const { stdout: networksStdout } = await execAsync(
+    'docker inspect $(hostname) -f "{{json .NetworkSettings.Networks }}"'
+  );
+  const networks = JSON.parse(networksStdout) as Record<string, unknown>;
+  const network = Object.keys(networks)[0];
+
   return new Promise((resolve) => {
     const dockerImage = "poc-device:chrome"; // must matches the image name in `./scripts/dev.sh`
     const containerPort = 5900;
     const hostPort = ++lastHostPort;
+
     const args = [
-      `run`,
-      `--env`,
+      "run",
+      "--env",
       `SCREEN_RESOLUTION=${width}x${height}x24`,
-      `--name`,
+      "--name",
       `poc-${hostPort}`,
-      `--publish`,
+      "--network",
+      network,
+      "--publish",
       `${hostPort}:${containerPort}`,
-      `--rm`,
+      "--rm",
       dockerImage,
-      `google-chrome`,
+      "google-chrome",
       `--app=${url}`,
     ];
     const docker = spawn("docker", args);
